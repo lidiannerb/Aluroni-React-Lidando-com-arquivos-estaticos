45e545ddd19c96571d121ba36cb97cc4afa9e170
Lidianne Barbosa <lidiannerb@gmail.com>
2022-06-06 13:09:12 -0300

    dynamically adding images

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,16 +1,16 @@
-import logo from "assets/logo.svg";
 import styles from "./Item.module.scss";
 import cardapio from "../itens.json";
+import classNames from "classnames";
 
 type Props = typeof cardapio[0];
 
 export default function Item (props: Props) {
-  const { title, description, category, size, serving, price } = props;
+  const { title, description, category, size, serving, price, photo } = props;
 
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt={title} />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
@@ -18,7 +18,10 @@ export default function Item (props: Props) {
           <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>
+          <div className={classNames({
+            [styles.item__tipo] : true,
+            [styles[`item__tipo__${category.label.toLowerCase()}`]] : true,
+          })}>
            {category.label}
           </div>
           <div className={styles.item__porcao}>
